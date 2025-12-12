de6078a9a98a21dde0cdde0a955076438feb897c
leylaheydarova <leylaheydarova.dev@gmail.com>
2025-12-12 22:09:24 +0400

    Edit action and its view

--- MyBlog/MyBlog.App/Areas/admin/Controllers/CategoryController.cs
+++ MyBlog/MyBlog.App/Areas/admin/Controllers/CategoryController.cs
@@ -1,8 +1,10 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MyBlog.App.DTOs.Category;
 using MyBlog.App.Services.Interfaces;
 
 namespace MyBlog.App.Areas.admin.Controllers
 {
+    [Area("Admin")]
     public class CategoryController : Controller
     {
         readonly ICategoryService _service;
@@ -12,11 +14,49 @@ namespace MyBlog.App.Areas.admin.Controllers
             _service = service;
         }
 
-        public IActionResult Index()
+        [HttpGet]
+        public async Task<IActionResult> Index()
         {
-            return View(_service.GetAll());
+            return View(await _service.GetAll());
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
         }
 
 
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(CategoryCreateDto dto)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(dto);
+            }
+            await _service.CreateAsync(dto);
+            return RedirectToAction(nameof(Index));
+        }
+
+        [HttpGet("edit/{id}")]
+        public async Task<IActionResult> Edit([FromRoute] int id)
+        {
+            var getDto = await _service.GetSingleAsync(id);
+            var updateDto = new CategoryUpdateDto() { Name = getDto.Name, Id = id };
+            return View(updateDto);
+        }
+
+        [HttpPost("edit/{id}")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit([FromRoute] int id, CategoryUpdateDto dto)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(dto);
+            }
+            await _service.UpdateAsync(id, dto);
+            return RedirectToAction(nameof(Index));
+        }
     }
 }
